6f44ef55da2b6a4e1bf5db9c1cc681d8a65e55cb
Arpit Gupta <glex.qsd@gmail.com>
2016-12-02 10:42:31 -0500

    WIP: code cleaning

--- fabric-manager/p4_compiler.py
+++ fabric-manager/p4_compiler.py
@@ -345,57 +345,7 @@ class PacketStream(object):
         self.update_p4_src()
 
 
-class FabricManagerConfig(object):
-    def __init__(self):
-        self.p4_src = ''
-        self.queries = []
 
-    def add_query(self, q):
-        self.queries.append(q)
-
-    def compile_dp(self):
-        out = ''
-        for q in self.queries:
-            q.compile_dp()
-
-        out += self.queries[0].p4_invariants
-
-        out += 'parser parse_out_header {\n\t'
-        for q in self.queries:
-            out += 'extract(out_header_'+str(q.qid)+');\n\t'
-
-        out += 'return parse_ethernet;\n}\n\n'
-
-        for q in self.queries:
-            out += q.p4_egress
-        for q in self.queries:
-            out += q.p4_utils
-        for q in self.queries:
-            out += q.p4_state
-
-        out += 'control ingress {\n\t'
-        for q in self.queries:
-            out += q.p4_control
-        out += '}\n\n'
-        out += 'control egress {\n\t'
-        for q in self.queries:
-            out += 'apply(encap_'+str(q.qid)+');'
-
-        out += '\n}\n\n'
-        self.p4_src = out
-        return out
-
-#
-fm = FabricManagerConfig()
-q1 = PacketStream(1).distinct(keys = ('sIP', 'dIP'))
-q2 = PacketStream(2).reduce(keys= ('dIP',))
-#q3 = PacketStream(fm.gc).distinct(keys = ('sIP', 'dIP')).reduce(keys= ('dIP',))
-
-
-
-#fm.add_query(q1)
-fm.add_query(q2)
-fm.compile_dp()
 #print [x.p4_invariants for x in fm.queries]
 #print fm.p4_src
 
