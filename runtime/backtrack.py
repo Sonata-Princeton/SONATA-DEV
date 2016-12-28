6daff4a7c6c9c14bdfc63c90ac0804dc179c08b2
Arpit Gupta <glex.qsd@gmail.com>
2016-12-27 21:23:25 -0500

    Phew....completed first iteration of recursive backtracking to learn the optimal refinement plan for a query tree

--- runtime/backtrack.py
+++ runtime/backtrack.py
@@ -65,7 +65,11 @@ def check_transit(prev, elem, costs):
     else:
         return False
 
-def get_refinement_plan(a, costs, plans):
+memoized_plans = {}
+ref_levels = range(0,36,8)
+
+def get_refinement_plan(final_level, a, costs, plans, subtree):
+    #print "Function called for", final_level, subtree
     root = 0
     level = 0
     levels = range(0,len(a)+1)
@@ -92,9 +96,41 @@ def get_refinement_plan(a, costs, plans):
                     is_transit_allowed = False
                     if ((prev_plan_id, plan_id), (prev,elem)) in costs:
                         is_transit_allowed = True
-                    #print curr_level, ((prev_plan_id, plan_id), (prev,elem)), is_transit_allowed
+                    #print curr_level, (prev_plan_id, plan_id), (prev,elem), is_transit_allowed
                     if is_transit_allowed:
                         #print curr_level, prev, elem, is_transit_allowed
+                        curr_plan_cost = 0
+                        if len(subtree.keys()) > 0:
+                            for qid in subtree:
+                                c = {}
+                                st = subtree[qid]
+                                ps = []
+                                fl = elem
+                                if qid not in memoized_plans:
+                                    memoized_plans[qid] = {}
+                                if elem not in memoized_plans[qid]:
+
+
+                                    for p1 in query_2_plans[qid]:
+                                        for p2 in query_2_plans[qid]:
+                                            for transit in query_2_cost[qid][(p1,p2)]:
+                                                c[((p1,p2), transit)] = query_2_cost[qid][(p1,p2)][transit]
+                                    #print c
+                                    for pid in query_2_plans[qid]:
+                                        for e in ref_levels:
+                                            if e > fl:
+                                                break
+                                            else:
+                                                ps.append((pid, e))
+
+                                    fp, cst = get_refinement_plan(fl, ps, c, query_2_plans[qid], st)
+                                    memoized_plans[qid][elem] = (fp, cst)
+                                else:
+                                    (fp, cst) = memoized_plans[qid][elem]
+
+                                curr_plan_cost += cst
+                                #print qid, fp, cst
+
                         prev_plan = node_2_all_plans[curr_level][(prev_plan_id, prev)]
                         if tuple(prev_plan) in plan_2_cost:
                             prev_plan_cost = plan_2_cost[tuple(prev_plan)]
@@ -102,7 +138,7 @@ def get_refinement_plan(a, costs, plans):
                             prev_plan_cost = get_plan_cost(tuple(prev_plan))
 
                         curr_plan = prev_plan+[(plan_id, elem)]
-                        curr_plan_cost = costs[((prev_plan_id, plan_id), (prev,elem))]+prev_plan_cost
+                        curr_plan_cost += costs[((prev_plan_id, plan_id), (prev,elem))]+prev_plan_cost
                         if (plan_id, elem) not in node_2_plan:
                             node_2_plan[(plan_id, elem)] = curr_plan
                             explore_further[next_level][(plan_id, elem)] = 0
@@ -126,10 +162,12 @@ def get_refinement_plan(a, costs, plans):
                         #print curr_plan, curr_plan_cost, node_2_plan
                         #print tuple(curr_plan)
                         plan_2_cost[tuple(curr_plan)] = curr_plan_cost
+    #print node_2_plan
     min_cost = 1000000
     final_plan = []
     for (plan, elem) in node_2_plan:
-        if elem == 32:
+        #print final_level, elem, elem == final_level
+        if elem == final_level:
             candidate_plan = node_2_plan[(plan, elem)]
             cost = plan_2_cost[tuple(candidate_plan)]
             print "Final Candidate Plans", candidate_plan, cost
@@ -145,32 +183,39 @@ def get_refinement_plan(a, costs, plans):
 if __name__ == '__main__':
     #print node_2_plan
     a = range(0,36,8)
+    plans = range(1,4)
+    query_tree = {1:{2:{}}}
     query_2_cost = {}
+    query_2_plans = {}
     for query_id in range(3):
         query_id += 1
+        query_2_plans[query_id] = plans
         if query_id not in query_2_cost:
             query_2_cost[query_id] = {}
-        plans = range(1,4)
+
         for p1 in plans:
             for p2 in plans:
                 query_2_cost[query_id][(p1,p2)] = generate_costs(a)
 
-    for query_id in query_2_cost:
+    for query_id in query_tree:
         costs = {}
+        subtree = query_tree[query_id]
         possibility_space = []
+        final_level = 32
         for plan_id in query_2_cost[query_id]:
             for transit in query_2_cost[query_id][plan_id]:
                 costs[(plan_id, transit)] = query_2_cost[query_id][plan_id][transit]
 
         for plan_id in plans:
             for elem in a:
-                possibility_space.append((plan_id, elem))
+                if elem > final_level:
+                    break
+                else:
+                    possibility_space.append((plan_id, elem))
 
-        #costs = query_2_cost[query_id][1]
-        #print query_id, possibility_space, costs
-        final_plan, cost = get_refinement_plan(possibility_space, costs, plans)
+        final_plan, cost = get_refinement_plan(final_level, possibility_space, costs, plans, subtree)
         print query_id, final_plan, cost
-        #break
+        break
 
     #print query_2_cost
 
