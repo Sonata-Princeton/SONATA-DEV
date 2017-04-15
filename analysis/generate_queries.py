991d432a3878490f03c13b8987164ddbc4058f58
Arpit Gupta <glex.qsd@gmail.com>
2017-04-15 10:28:27 -0400

    added some queries for sloc analysis

--- analysis/generate_queries.py
+++ analysis/generate_queries.py
@@ -44,8 +44,11 @@ def compile_queries(query):
 
 
 if __name__ == '__main__':
+    # Follows Table 1 in the paper
+
+    # Traffic anomaly detection (All UDP traffic)
     q1 = (PacketStream(1)
-          # .filter(filter_keys=('proto',), func=('eq', 6))
+          .filter(filter_keys=('proto',), func=('eq', 17))
           .map(keys=('dIP', 'sIP'))
           .distinct(keys=('dIP', 'sIP'))
           .map(keys=('dIP',), map_values=('count',), func=('eq', 1,))
@@ -53,4 +56,87 @@ if __name__ == '__main__':
           .filter(filter_vals=('count',), func=('geq', '99.99'))
           .map(keys=('dIP',))
           )
+
+    # heavy hitter detection
+    q2 = (PacketStream(2)
+          .map(keys=('dIP', 'dPort','sPort','sIP'), values=('nBytes',))
+          .reduce(keys=('dIP', 'dPort','sPort','sIP',), func=('sum',))
+          .filter(filter_vals=('nBytes',), func=('geq', '99'))
+          .map(keys=('dIP',))
+          )
+
+    # super spreader detection
+    q3 = (PacketStream(3)
+          .map(keys=('dIP', 'sIP'))
+          .distinct(keys=('dIP', 'sIP'))
+          .map(keys=('sIP',), map_values=('count',), func=('eq', 1,))
+          .reduce(keys=('sIP',), func=('sum',))
+          .filter(filter_vals=('count',), func=('geq', '99.99'))
+          .map(keys=('sIP',))
+          )
+
+    # ssh brute forcing
+    q4 = (PacketStream(4)
+          # .filter(filter_keys=('proto',), func=('eq', 6))
+          .map(keys=('dIP', 'sIP', 'nBytes'))
+          .distinct(keys=('dIP', 'sIP', 'nBytes'))
+          .map(keys=('dIP','nBytes'), map_values=('count',), func=('eq', 1,))
+          .reduce(keys=('dIP','nBytes'), func=('sum',))
+          .filter(filter_vals=('count',), func=('geq', '99'))
+          .map(keys=('dIP',))
+          )
+    # DNS TTL change detection
+    q5 = (PacketStream(5)
+          .filter(filter_keys=('sPort',), func=('eq', 53))
+          .map(keys=('domain', 'ttl'), map_values=('count',), func=('eq', 1,))
+          .reduce(keys=('domain', 'ttl'), func=('sum',))
+          .filter(filter_vals=('count',), func=('geq', '99'))
+          .map(keys=('domain',))
+          )
+
+    # IP-2-Domain Anomaly
+    q6 = (PacketStream(6)
+          .filter(filter_keys=('sPort',), func=('eq', 53))
+          .map(keys=('qname', 'rdata'))
+          .distinct()
+          .map(keys=('rdata',), map_values=('count',), func=('eq', 1,))
+          .reduce(keys=('rdata',), func=('sum',))
+          .filter(filter_vals=('count',), func=('geq', '99'))
+          .map(keys=('rdata',))
+          )
+
+
+    # Domain-2-IP Anomaly
+    q7 = (PacketStream(7)
+          .filter(filter_keys=('sPort',), func=('eq', 53))
+          .map(keys=('qname', 'rdata'))
+          .distinct()
+          .map(keys=('qname',), map_values=('count',), func=('eq', 1,))
+          .reduce(keys=('qname',), func=('sum',))
+          .filter(filter_vals=('count',), func=('geq', '99'))
+          .map(keys=('qname',))
+          )
+
+    # Loop freedom detection
+    # q4 = (PacketStream(4)
+    #       .map(keys=('flowID', 'path'))
+    #       .distinct()
+    #       .map(keys=('qname',), map_values=('count',), func=('eq', 1,))
+    #       .reduce(keys=('qname',), func=('sum',))
+    #       .filter(filter_vals=('count',), func=('geq', '99'))
+    #       .map(keys=('qname',))
+    #       )
+
+
+    # Congested link detection
+
+    # Silent packet drop detection
+
+    # Path Conformance detection
+
+
+
+
+
+
     compile_queries(q1)
