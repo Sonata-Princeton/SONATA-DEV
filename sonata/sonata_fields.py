79549a227fdcc0637dda81284054e0dac0d61394
Ankita Pawar <ankscircle@gmail.com>
2017-08-11 14:14:30 -0400

    WIP: config file for Sonata

--- sonata/sonata_fields.py
+++ sonata/sonata_fields.py
@@ -2,10 +2,16 @@
 
 class SonataField(object):
     sonata_name = ""
-    targets = []
-    target_2_name = []
+    target_name = ""
+    size = 0
+    layer = None
 
-    def __init__(self, sonata_name, targets, target_2_name):
+    def __init__(self, layer, sonata_name, target_name, size):
+
+        self.layer = layer
         self.sonata_name = sonata_name
-        self.targets = targets
-        self.target_2_name = target_2_name
+        self.target_name = target_name
+        self.size = size
+
+    def __repr__(self):
+        return "SonataField(s="+self.sonata_name+", t="+self.target_name+", size="+str(self.size)+")"
