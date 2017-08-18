20e5ae166b9930b00442c2789aa6a9c118f7f9d4
Arpit Gupta <glex.qsd@gmail.com>
2017-08-18 16:12:49 -0400

    starting a new analysis script (will move to different branch later)

---
+++ analysis/performance_eval/generate_cost_matrix.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+#  Author:
+#  Arpit Gupta (arpitg@cs.princeton.edu)
+
+import pickle
+import time
+import datetime
+import math
+
+from sonata.query_engine.sonata_queries import *
+from sonata.core.training.utils import get_spark_context_batch, create_spark_context
+from sonata.core.integration import Target
+from sonata.core.refinement import apply_refinement_plan, get_refined_query_id, Refinement
+from sonata.core.training.hypothesis.hypothesis import Hypothesis
+
+def parse_log_line(logline):
+    return tuple(logline.split(","))
+
+def generate_graph(sc, query):
+    TD_PATH = '/mnt/anon_all_flows_5min.csv'
+    TD_PATH = '/mnt/caida_5min.csv'
+
+    # TD_PATH = '/mnt/anon_all_flows_1min.csv'
+    # TD_PATH = '/mnt/anon_all_flows_5min.csv/part-00500'
+    # TD_PATH = '/mnt/anon_all_flows_1min.csv'
+    # TD_PATH = '/home/vagrant/dev/data/anon_all_flows_1min.csv/part-00496'
+    # TD_PATH = '/mnt/anon_all_flows_1min.csv/part-00496'
+    # TD_PATH = '/home/vagrant/dev/data/anon_all_flows_1min.csv'
+
+    flows_File = TD_PATH
+    T = 10
+    if query.qid == 1:
+        training_data = (sc.textFile(flows_File)
+                         .map(parse_log_line)
+                         .map(lambda s:tuple([int(math.ceil(int(s[0])/T))]+(list(s[1:]))))
+                         .filter(lambda (ts,sIP,sPort,dIP,dPort,nBytes,proto,sMac,dMac): str(proto) == '17')
+                         )
+
+    elif query.qid == 2:
+        training_data = (sc.textFile(flows_File)
+                         .map(parse_log_line)
+                         .map(lambda s: tuple([int(math.ceil(int(s[0])/T))]+(list(s[1:]))))
+                         )
+
+    if query.qid == 3:
+        training_data = (sc.textFile(flows_File)
+                         .map(parse_log_line)
+                         .map(lambda s:tuple([int(math.ceil(int(s[0])/T))]+(list(s[1:]))))
+                         .filter(lambda (ts,sIP,sPort,dIP,dPort,nBytes,proto,sMac,dMac): str(proto) == '6')
+                         .filter(lambda (ts,sIP,sPort,dIP,dPort,nBytes,proto,sMac,dMac): str(sPort) == '22' or str(dPort) == '22')
+                         .map(lambda (ts,sIP,sPort,dIP,dPort,nBytes,proto,sMac,dMac): (ts,sIP,sPort,dIP,dPort,int(nBytes)/10,proto,sMac,dMac))
+                         )
+    elif query.qid == 4:
+        # only applied over DNS response traffic
+        training_data = (sc.textFile(flows_File)
+                         .map(parse_log_line)
+                         .map(lambda s:tuple([int(math.ceil(int(s[0])/T))]+(list(s[1:]))))
+                         .filter(lambda (ts,sIP,sPort,dIP,dPort,nBytes,proto,sMac,dMac): str(proto) == '17')
+                         .filter(lambda (ts,sIP,sPort,dIP,dPort,nBytes,proto,sMac,dMac): str(sPort) == '53')
+                         )
+    elif query.qid == 5:
+        training_data = (sc.textFile(flows_File)
+                         .map(parse_log_line)
+                         .map(lambda s:tuple([int(math.ceil(int(s[0])/T))]+(list(s[1:]))))
+                         .filter(lambda (ts,sIP,sPort,dIP,dPort,nBytes,proto,sMac,dMac): str(proto) == '17')
+                         .filter(lambda (ts,sIP,sPort,dIP,dPort,nBytes,proto,sMac,dMac): str(sPort) == '19')
+                         )
+    elif query.qid == 6:
+        # Response traffic for NTP protocol
+        training_data = (sc.textFile(flows_File)
+                         .map(parse_log_line)
+                         .map(lambda s:tuple([int(math.ceil(int(s[0])/T))]+(list(s[1:]))))
+                         .filter(lambda (ts,sIP,sPort,dIP,dPort,nBytes,proto,sMac,dMac): str(proto) == '17')
+                         )
+
+    print "Collecting the training data for the first time ...", training_data.take(2)
+    training_data = sc.parallelize(training_data.collect())
+    print "Collecting timestamps for the experiment ..."
+    timestamps = training_data.map(lambda s: s[0]).distinct().collect()
+    print "#Timestamps: ", len(timestamps)
+    target = Target()
+    refinement_object = Refinement(query, target)
+    refinement_object.update_filter(training_data)
+    hypothesis = Hypothesis(query, sc, training_data, timestamps,refinement_object, target)
+    G = hypothesis.G
+    fname = 'data/hypothesis_graph_'+str(query.qid)+'_'+str(datetime.datetime.fromtimestamp(time.time()))+'.pickle'
+
+    # dump the hypothesis graph: {ts:G[ts], ...}
+    print "Dumping graph to", fname
+    with open(fname, 'w') as f:
+        pickle.dump(G, f)
+
+if __name__ == '__main__':
+    # original reflection attack query for all UDP traffic
+    q1 = (PacketStream(1)
+          # .filter(filter_keys=('proto',), func=('eq', 6))
+          .map(keys=('dIP', 'sIP'))
+          .distinct(keys=('dIP', 'sIP'))
+          .map(keys=('dIP',), map_values=('count',), func=('eq', 1,))
+          .reduce(keys=('dIP',), func=('sum',))
+          .filter(filter_vals=('count',), func=('geq', '99.9'))
+          .map(keys=('dIP',))
+          )
+
+    # flow distribution
+    q2 = (PacketStream(2)
+          # .filter(filter_keys=('proto',), func=('eq', 6))
+          .map(keys=('dIP', 'sIP'), values=('nBytes',))
+          .reduce(keys=('dIP','sIP',), func=('sum',))
+          .filter(filter_vals=('nBytes',), func=('geq', '99'))
+          .map(keys=('dIP',))
+          )
+
+    # ssh brute forcing
+    q3 = (PacketStream(3)
+          # .filter(filter_keys=('proto',), func=('eq', 6))
+          .map(keys=('dIP', 'sIP', 'nBytes'))
+          .distinct(keys=('dIP', 'sIP', 'nBytes'))
+          .map(keys=('dIP','nBytes'), map_values=('count',), func=('eq', 1,))
+          .reduce(keys=('dIP','nBytes'), func=('sum',))
+          .filter(filter_vals=('count',), func=('geq', '99'))
+          .map(keys=('dIP',))
+          )
+
+    # reflection attack query (DNS)
+    q4 = (PacketStream(4)
+          # .filter(filter_keys=('proto',), func=('eq', 6))
+          .map(keys=('dIP', 'sIP'))
+          .distinct(keys=('dIP', 'sIP'))
+          .map(keys=('dIP',), map_values=('count',), func=('eq', 1,))
+          .reduce(keys=('dIP',), func=('sum',))
+          .filter(filter_vals=('count',), func=('geq', '99'))
+          .map(keys=('dIP',))
+          )
+
+    # reflection attack query (NTP)
+    q5 = (PacketStream(5)
+          # .filter(filter_keys=('proto',), func=('eq', 6))
+          .map(keys=('dIP', 'sIP'))
+          .distinct(keys=('dIP', 'sIP'))
+          .map(keys=('dIP',), map_values=('count',), func=('eq', 1,))
+          .reduce(keys=('dIP',), func=('sum',))
+          .filter(filter_vals=('count',), func=('geq', '99'))
+          .map(keys=('dIP',))
+          )
+
+    q6 = (PacketStream(6)
+          # .filter(filter_keys=('proto',), func=('eq', 6))
+          .map(keys=('dIP', 'sIP'))
+          .distinct(keys=('dIP', 'sIP'))
+          .map(keys=('sIP',), map_values=('count',), func=('eq', 1,))
+          .reduce(keys=('sIP',), func=('sum',))
+          .filter(filter_vals=('count',), func=('geq', '99.99'))
+          .map(keys=('sIP',))
+          )
+
+    queries = [q1]
+    # queries = [q2]
+    sc = create_spark_context()
+    for q in queries:
+        generate_graph(sc, q)
