e9292bce21e14ae14fb5398110804aa7d2d15442
Ankita Pawar <ankscircle@gmail.com>
2017-08-19 14:28:24 -0400

    clean up

--- cleanup.sh
+++ cleanup.sh
@@ -35,4 +35,4 @@ sudo ps -ef | grep test | grep -v grep | awk '{print $2}' | sudo xargs kill -9 &
 sudo ps -ef | grep query_ | grep -v grep | awk '{print $2}' | sudo xargs kill -9 && sudo mn -c && sudo ps -ef | grep simple_switch | grep -v grep | awk '{print $2}' | sudo xargs kill -9
 sudo ps -ef | grep stream | grep -v grep | awk '{print $2}' | sudo xargs kill -9 && sudo mn -c && sudo ps -ef | grep simple_switch | grep -v grep | awk '{print $2}' | sudo xargs kill -9
 
-echo "" > sonata/tests/demos/reflection_dns/graph/emitter.log
+echo "" > sonata/examples/reflection_dns/graph/emitter.log
